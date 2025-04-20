55ba58190d85eba568ffd198c419b24a74adf307
Davis Hammon <accounts@risingtiger.com>
2025-04-19 22:00:23 -0600

    refactor: remove unused methods and clean up home view code

--- lazy/views/home/home.ts
+++ lazy/views/home/home.ts
@@ -55,7 +55,6 @@ class VHome extends HTMLElement {
 
 
 	async connectedCallback() {
-
 		await $N.CMech.ViewConnectedCallback(this)
 		this.dispatchEvent(new Event('hydrated'));
 	}
@@ -74,17 +73,9 @@ class VHome extends HTMLElement {
 
 
 
-	visibled = () => new Promise<void>(async (res) => { 
-		const a = await testdb_a() as any[]
-		console.log(a)
 
-		/*
-		await new Promise<void>(res => setTimeout(() => res(), 1000))
-		const b = await testdb_b() as any[]
-		console.log(b)
-		*/
-		res()
-	})
+	kd = () =>  {
+	}
 
 
 
@@ -103,6 +94,24 @@ class VHome extends HTMLElement {
 			const returndata = await $N.FetchLassie("/api/xen/admin/" + api, { method } ) as any
 			if (returndata.return_str) {
 				this.s.admin_return_str = returndata.return_str.includes("--") ? returndata.return_str.split("--") : returndata.return_str
+			}
+
+			this.sc()
+		}
+	}
+
+
+
+
+	async plaidetc(api:str, method: "POST" | "GET" = "GET") {
+
+		if (confirm("Are you sure you want to plaid: " + api)) {
+
+			const returndata = await $N.FetchLassie("/api/xen/finance/plaid/" + api, { method } ) as any
+
+			if (api === "create_link_token") {
+				localStorage.setItem("plaid_link_token", returndata.link_token)
+				alert ("Link token created: " + returndata.link_token)
 			}
 
 			this.sc()
@@ -177,155 +186,6 @@ class VHome extends HTMLElement {
 
 
 customElements.define('v-home', VHome);
-
-
-
-
-const testdb_a = () => new Promise(async (resolve, reject) => {
-
-	const db = await openindexeddb()
-
-	let  transactions: any[]      = [];
-	let  cats:any[]               = [];
-	let  sources:any[]            = [];
-        
-	const transaction             = db.transaction(['transactions', 'cats', 'sources'], 'readonly');
-	const transaction_store       = transaction.objectStore('transactions');
-	const cat_store               = transaction.objectStore('cats');
-	const source_store            = transaction.objectStore('sources');
-
-	const t1 = performance.now()
-
-	const cats_request    = cat_store.getAll();
-	const sources_request = source_store.getAll();
-	
-	const catsPromise = new Promise<any[]>((resolveC, rejectC) => {
-		cats_request.onsuccess = () => {
-			resolveC(cats_request.result);
-		};
-		cats_request.onerror = () => {
-			rejectC('Error fetching cats');
-		};
-	});
-
-	const sourcesPromise = new Promise<any[]>((resolveC, rejectC) => {
-		sources_request.onsuccess = () => {
-			resolveC(sources_request.result);
-		};
-		sources_request.onerror = () => {
-			rejectC('Error fetching sources');
-		};
-	});
-
-	let count = 0;
-	transaction_store.openCursor().onsuccess = (event) => {
-		const cursor = (event.target as IDBRequest<IDBCursorWithValue>).result;
-		
-		if (cursor && count < 500) {
-			const transaction = cursor.value;
-			transactions.push(transaction);
-			count++;
-			cursor.continue();
-		} else {
-
-			Promise.all([catsPromise, sourcesPromise]).then((results) => {
-				const t2 = performance.now()
-				console.log("testdb_a time: " + (t2 - t1));
-				cats = results[0];
-				sources = results[1];
-				resolve({transactions, cats, sources});
-			}).catch(error => {
-				console.error(error);
-				reject(error);
-			});
-		}
-	}
-})
-
-
-
-
-const testdb_b = () => new Promise(async (resolve, reject) => {
-
-	const db = await openindexeddb()
-
-	let  transactions: any[]      = [];
-	let  cats:any[]               = [];
-	let  sources:any[]            = [];
-	let  cat_ids:Set<string>      = new Set<string>();
-	let  source_ids:Set<string>   = new Set<string>();
-        
-	const transaction             = db.transaction(['transactions', 'cats','sources'], 'readonly');
-	const transaction_store       = transaction.objectStore('transactions');
-	const cat_store               = transaction.objectStore('cats');
-	const source_store            = transaction.objectStore('sources');
-
-	const t1 = performance.now()
-
-	transaction_store.openCursor().onsuccess = (event) => {
-		const cursor = (event.target as IDBRequest<IDBCursorWithValue>).result;
-		
-		if (cursor) {
-			const transaction = cursor.value;
-			transactions.push(transaction);
-			
-			// Process cat if not already processed
-			if (!cat_ids.has(transaction.cat)) {
-				const cat_request = cat_store.get(transaction.cat);
-				
-				cat_request.onsuccess = () => {
-					cats.push(cat_request.result)	
-					cat_ids.add(transaction.cat)
-				};
-				
-				cat_request.onerror = () => {
-					console.error('Error retrieving cat:', cat_request.error);
-				};
-			}
-			
-			// Process source if not already processed
-			if (!source_ids.has(transaction.source)) {
-				const source_request = source_store.get(transaction.source);
-				
-				source_request.onsuccess = () => {
-					sources.push(source_request.result)	
-					source_ids.add(transaction.source)
-				};
-				
-				source_request.onerror = () => {
-					console.error('Error retrieving source:', source_request.error);
-				};
-			}
-
-			cursor.continue();
-		} else {
-			const t2 = performance.now()
-			console.log("-testdb_b time: " + (t2 - t1));
-
-			resolve({transactions, cats, sources});
-		}
-	}
-})
-
-
-
-
-const openindexeddb = () => new Promise<IDBDatabase>(async (res,_rej)=> {
-
-	let dbconnect = indexedDB.open('xenition', 4)
-
-	dbconnect.onerror = (event:any) => { 
-		console.log("IndexedDB Error - " + event.target.errorCode)
-	}
-
-	dbconnect.onsuccess = async (event: any) => {
-		event.target.result.onerror = (event:any) => {
-			console.log("IndexedDB Error - " + event.target.errorCode)
-		}
-		const db = event.target.result
-		res(db)
-	}
-})
 
 
 export {  }
