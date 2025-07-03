9c4f42fe79c6e2a7798be06fb0c643b02abd5e37
Davis Hammon <accounts@risingtiger.com>
2025-07-02 23:02:52 -0600

    refactor: update Apple transaction parsing to use clipboard image

--- lazy/libs/addtr_apple.ts
+++ lazy/libs/addtr_apple.ts
@@ -8,90 +8,19 @@ declare var $N: $NT;
 
 export const ParseApple = async (sources:SourceT[]) => new Promise<NewTransactionT[]>(async (res, rej) => {
 
-	const clipboardText = `
 
-8:06
-Done
+	const clipboardImg = await navigator.clipboard.read();
 
--123
+	const imgclip = clipboardImg.find(item => item.types.includes('image/png'));
 
-
-Bee's Marketplace
-Pending - Colorado City
-3 hours ago
-$12.75
-2%
-
-
-Red Brick take N bake... $10.86
-Pending - Colorado City
-5 hours ago
-2%
-へ
-
-Amazon Web Services
-Card Number Used
-Yesterday
-$1.11
-1%
-
-
-InfluxData
-Card Number Used
-Yesterday
-$0.05
-1%
-
-G
-Google
-Card Number Used
-Yesterday
-$22.72
-1%
-
-G
-Google
-Card Number Used
-Yesterday
-$17.88
-1%
-
-Beans
-Brews
-Beans & Brews Coffee...
-Colorado City, AZ
-Monday
-$4.85
-2%
-＞
-
-Essential Coffee
-Colorado City, AZ
-Monday
-$5.75
-2%
-
-
-YouTube
-Card Number Used
-Monday
-$5.42
-1%
-
-
-Hildale-Colorado Cit... $135.03
-Card Number Used
-Mondak
-1%
-	`
-
-
-	//const clipboardText = await navigator.clipboard.readText();
-	
-	if (!clipboardText.includes('Done') || !clipboardText.includes('123')) { 
+	if (!imgclip) {
 		rej('Clipboard text must contain both "Done" and "123"'); 
-		return; 
+		return;
 	}
+
+	const blob = await imgclip.getType('image/png');
+	const imgUrl = URL.createObjectURL(blob);
+	
 
 	const now = new Date();
 	const timezone_offset = -(now.getTimezoneOffset() / 60);
@@ -104,24 +33,21 @@ Mondak
 	const minutes = String(now.getMinutes()).padStart(2, '0');
 	const seconds = String(now.getSeconds()).padStart(2, '0');
 	const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+
 	
 	const offset_sign = timezone_offset >= 0 ? '+' : '-';
 	const offset_hours = String(Math.abs(Math.floor(timezone_offset))).padStart(2, '0');
 	const offset_minutes = String(Math.abs((timezone_offset % 1) * 60)).padStart(2, '0');
 	
-	const relative_date = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}${offset_sign}${offset_hours}:${offset_minutes}`;
-	console.log("relative_date", relative_date, "timezone_offset", timezone_offset);
+	const localnow = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}${offset_sign}${offset_hours}:${offset_minutes}`;
 	
-	const splitText = clipboardText.split("123");
-	const apple_data_str = splitText[1];
 	
-	const body = {apple_data: apple_data_str, relative_date, timezone_offset}
+	const body = {localnow, timezone_offset}
 	const httpopts = {method: "POST", body: JSON.stringify(body)}
 
 	const transactions:NewTransactionT[] = []
 	const r = await $N.FetchLassie("/api/xen/finance/ai/parse_apple", httpopts )
 	if (!r.ok || ( r.data as any ).length === 0) {
-		console.error("Error fetching data:");
 		rej();
 		return;
 	}
