24151f66bcb70802090b8dc168f95f24b4236205
Davis Hammon <accounts@risingtiger.com>
2025-07-02 19:32:44 -0600

    refactor: update ParseApple function to handle Apple transaction parsing with improved error handling

--- lazy/libs/addtr_apple.ts
+++ lazy/libs/addtr_apple.ts
@@ -1,45 +1,57 @@
 
 
-import { CatT, TagT } from "../../defs.js"
-import { InputModeE, ModelT, StateT } from "./addtr_defs.js"
-
-
+import { NewTransactionT } from "./addtr_defs.js"
+import { SourceT } from '../../defs.js'
 import { $NT } from "../../defs_client_symlink.js"
 declare var $N: $NT;
 
 
-export const ParseApple = async () => new Promise<void>(async (res, _rej) => {
+export const ParseApple = async (sources:SourceT[]) => new Promise<NewTransactionT[]>(async (res, rej) => {
 
 	const clipboardText = await navigator.clipboard.readText();
 	
-	const body = {apple_data: clipboardText}
+	if (!clipboardText.includes('Done') || !clipboardText.includes('123')) { 
+		rej('Clipboard text must contain both "Done" and "123"'); 
+		return; 
+	}
+
+	const relative_date = ; 
+	const timezone_offset = 0
+	
+	const splitText = clipboardText.split("123");
+	const apple_data_str = splitText[1];
+	
+	const body = {apple_data: apple_data_str}
 	const httpopts = {method: "POST", body: JSON.stringify(body)}
 
-	const r = await $N.FetchLassie("/api/xen/finance/ai/parse_apple", httpopts ) as any
-	if (r.ok && !r.ok) {   return;   }
+	const transactions:NewTransactionT[] = []
+	const r = await $N.FetchLassie("/api/xen/finance/ai/parse_apple", httpopts )
+	if (!r.ok || ( r.data as any ).length === 0) {
+		console.error("Error fetching data:");
+		rej();
+		return;
+	}
 
 
-	/*
-date,merchant,amount
-2025-05-02,Espresso Creek,5.15
-2025-05-02,Amazon Web Services,1.11
-2025-05-01,Bee's Marketplace,26.20
-2025-05-01,Etsy,59.00
-2025-05-01,Parallels,4.43
-2025-05-01,Google,23.44
-2025-05-01,Basic American Supply,14.11
-	*/
+	for (let i = 0; i < (r.data as any[]).length; i++) { 
+		const nt = (r.data as any[])[i];
+		
+		const transaction:NewTransactionT = {
+			amount: nt.amount,
+			date: nt.date,
+			catref: null,
+			merchant: nt.merchant,
+			merchant_long: nt.merchant,
+			simplified_merchant: nt.merchant,
+			notes: nt.notes || '',
+			source: sources.find(s=>s.id==='7688adbc-13ef-469f-81d7-1e02098d2d06')!, 
+			tags: [],
+			sheets_id: null
+		}
+		
+		transactions.push(transaction);
+	}
 
-	//amount,date (as UTC timestamp in seconds),merchant,notes (always ''),preset_area_id (always null),preset_cat_name (always null),source_id (always '7688adbc-13ef-469f-81d7-1e02098d2d06'),tags (always []),ynab_id (always null)
-
-
-	res()
+	res(transactions)
 })
-
-
-
-
-
-
-
 
